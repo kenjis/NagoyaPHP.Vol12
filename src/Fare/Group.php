de7abdc4e633a052da61fcc3f50e78f4f2cabe83
kenjis <kenji.uui@gmail.com>
2018-04-06 00:33:27 +0900

    feat: Group fare

---
+++ src/Fare/Group.php
@@ -0,0 +1,85 @@
+<?php
+namespace NagoyaPhp\Vol12\Fare;
+
+class Group
+{
+    /**
+     * @var Adult[]
+     */
+    private $adults = [];
+
+    /**
+     * @var Child[]
+     */
+    private $children = [];
+
+    /**
+     * @var Infant[]
+     */
+    private $infants = [];
+
+    /**
+     * @var int
+     */
+    private $maxFreeInfantsPerAdult = 2;
+
+    public function add(Fare $one)
+    {
+        switch (true) {
+            case $one instanceof Adult:
+                $this->adults[] = $one;
+                break;
+            case $one instanceof Child:
+                $this->children[] = $one;
+                break;
+            case $one instanceof Infant:
+                $this->infants[] = $one;
+                break;
+        }
+    }
+
+    public function calc()
+    {
+        $total = 0;
+
+        $this->setFreeInfants();
+
+        $all = array_merge($this->adults, $this->children, $this->infants);
+
+        foreach ($all as $one) {
+            $total += $one->calc();
+        }
+
+        return $total;
+    }
+
+    private function setFreeInfants()
+    {
+        if (empty($this->infants)) {
+            return;
+        }
+
+        $this->sortInfants();
+
+        $maxFreeInfants = $this->countAdults() * $this->maxFreeInfantsPerAdult;
+
+        for ($i = 0; $i < $maxFreeInfants; $i++) {
+            $this->infants[$i]->withAdult();
+        }
+    }
+
+    private function sortInfants()
+    {
+        usort($this->infants, function ($a, $b) {
+            if ($a->calc() === $b->calc()) {
+                return 0;
+            }
+            return ($a->calc() < $b->calc()) ? 1 : -1;
+        });
+    }
+
+    private function countAdults()
+    {
+        return count($this->adults);
+    }
+}
---
+++ tests/Fare/GroupTest.php
@@ -0,0 +1,87 @@
+<?php
+namespace NagoyaPhp\Vol12\Fare;
+
+use PHPUnit\Framework\TestCase;
+
+class GroupTest extends TestCase
+{
+    public function test_料金グループに大人200円1人の場合、料金は200円()
+    {
+        $adultNormalFare = 200;
+        $adult = new Adult($adultNormalFare);
+        $group = new Group();
+        $group->add($adult);
+        $this->assertEquals(200, $group->calc());
+    }
+
+    public function test_料金グループに大人200円2人の場合、料金は400円()
+    {
+        $adultNormalFare = 200;
+        $adult1 = new Adult($adultNormalFare);
+        $adult2 = new Adult($adultNormalFare);
+        $group = new Group();
+        $group->add($adult1);
+        $group->add($adult2);
+        $this->assertEquals(400, $group->calc());
+    }
+
+    public function test_幼児は同行者の大人1名につき2名まで無料()
+    {
+        $adultNormalFare = 200;
+        $adult = new Adult($adultNormalFare);
+        $infant1 = new Infant($adultNormalFare);
+        $infant2 = new Infant($adultNormalFare);
+
+        $group = new Group();
+        $group->add($adult);
+        $group->add($infant1);
+        $group->add($infant2);
+
+        $fare = $group->calc();
+
+        $this->assertEquals(200, $fare);
+    }
+
+    public function test_幼児3名と大人1名の場合、大人料金プラス幼児料金()
+    {
+        $adultNormalFare = 200;
+        $adult = new Adult($adultNormalFare);
+        $infant1 = new Infant($adultNormalFare);
+        $infant2 = new Infant($adultNormalFare);
+        $infant3 = new Infant($adultNormalFare);
+
+        $group = new Group();
+        $group->add($adult);
+        $group->add($infant1);
+        $group->add($infant2);
+        $group->add($infant3);
+
+        $fare = $group->calc();
+
+        $this->assertEquals(300, $fare);
+    }
+
+    public function test_大人定期券あり、子供通常、幼児通常、幼児福祉割引2人の場合、料金は170円()
+    {
+        $adultNormalFare = 210;
+        $adult = new Adult($adultNormalFare);
+        $adult->withPass();
+        $child = new Child($adultNormalFare);
+        $infant1 = new Infant($adultNormalFare);
+        $infant2 = new Infant($adultNormalFare);
+        $infant2->withWelfare();
+        $infant3 = new Infant($adultNormalFare);
+        $infant3->withWelfare();
+
+        $group = new Group();
+        $group->add($adult);
+        $group->add($child);
+        $group->add($infant2);
+        $group->add($infant3);
+        $group->add($infant1);
+
+        $fare = $group->calc();
+
+        $this->assertEquals(170, $fare);
+    }
+}
